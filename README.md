013957a18eef665570d11406e4d92c2917faf1c1
LightAndTime <87947799+LightAndTime@users.noreply.github.com>
2022-01-09 16:54:46 +0100

    upload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Block-Placer
+# Block placer
+inspired by https://github.com/DeltaNedas/unit-factory
+
+### Done
+-basic concept
+
+### To do
+-fill option
+
+-anti-cheat, i don't want this to be used to beat the campaign
+
+### **absolute masterpiece**
+![image](https://user-images.githubusercontent.com/87947799/148459375-c69bc82f-dd02-427d-b7e2-2e9de523e17d.png)
---
+++ scripts/main.js
@@ -0,0 +1,141 @@
+
+const ui = require("ui-lib/library");
+var dialog = null, button = null;
+
+//change to show ALL blocks (not racommended)
+const showAllBlocks = false;
+
+var bRot = 1;
+const maxRot = 4; //slider doesn't accept 0 as input, so to have rotation 0 set it to 4
+
+var bType = Blocks.coreNucleus;
+var bTeam = Vars.state.rules.defaultTeam;
+const bPos = new Vec2(-1, -1);
+
+function placeLocal() {
+
+	if (Vars.world.tile(bPos.x, bPos.y)) {
+		
+		Vars.world.tile(bPos.x, bPos.y).setNet(bType, bTeam, bRot);
+		
+	} else {
+		Call.sendMessage("[scarlet]error: illegal coordinates");
+		Log.err("impossible to place " + bType + " at x: " + bPos.x + " y: " + bPos.y);
+	}
+
+}
+
+function placeRemote() {
+	
+	if (Vars.world.tile(bPos.x, bPos.y)) {
+		
+		const code = [
+			"Vars.world.tile(" + bPos.x + ", " + bPos.y + ").setNet(Vars.content.getByID(ContentType.block, " + bType.id + "), Team.all[" + bTeam.id + "], " + bRot + ")"
+		].join("");
+		
+		Call.sendChatMessage("/js " + code);
+		
+	} else {
+		Call.sendMessage("[scarlet]error: illegal coordinates")
+		Log.err("impossible to place " + bType + " at x: " + bPos.x + " y: " + bPos.y)
+	}
+}
+
+function place() {
+	(Vars.net.client() ? placeRemote : placeLocal)();
+}
+
+ui.onLoad(() => {
+	dialog = new BaseDialog("Place a block");
+	const table = dialog.cont;
+
+	table.label(() => bType.localizedName);
+
+	table.row();
+
+	table.pane(list => {
+		
+		var i = 0;
+		const blocks = Vars.content.blocks();
+		blocks.sort();
+		
+		var j;
+		var maxLine;
+		//what the... it works tho... kinda
+		if ((j = Math.floor((Core.graphics.width / Vars.content.blocks().size) * 2)) == 0) { maxLine = 1 } else { maxLine = j };
+			
+		blocks.each(block => {
+			
+			//sort out unusefull blocks/buggy blocks (to turn off set "showAllBlocks" to true)
+			if ((block == "build1" || block == "build2" || block == "build3" || block == "build4" ||
+				block == "build5" || block == "build6" || block == "build7" || block == "build8" ||
+				block == "build9" || block == "build10" || block == "build11" || block == "build12" ||
+				block == "build13" || block == "build14" || block == "build15" || block == "build16" ||
+				block == "legacy-mech-pad" || block == "legacy-unit-factory" || block == "legacy-unit-factory-air" || block == "legacy-unit-factory-ground" ||
+				block == "air" || block == "empty") && !showAllBlocks) { return }
+			
+			if (i++ % maxLine == 0) {
+				list.row();
+			}
+			
+			const icon = new TextureRegionDrawable(block.icon(Cicon.full));
+			list.button(icon, () => {
+				bType = block;
+				button.style.imageUp = icon;
+			}).size(128);
+			
+		});
+
+	}).top().center();
+	
+	table.row();
+	
+	const rotDial = table.table().center().bottom().get();
+	var rotSlider, rotField;
+	rotDial.defaults().left();
+	
+	rotSlider = rotDial.slider(1, maxRot, bRot, num => {
+		bRot = num;
+		rotField.text = num;
+	}).get();
+	
+	rotDial.add(" rotation: ");
+	
+	rotField = rotDial.field(" " + bRot, text => {
+		bRot = parseInt(text);
+		rotSlider.value = bRot;
+	}).get();
+	rotField.validator = text => !isNaN(parseInt(text));
+	
+	table.row();
+	
+	var posDial;
+	posDial = table.button("Set position", () => {
+		dialog.hide();
+		ui.click((screen, world) => {
+			bPos.set(Math.round(world.x/8), Math.round(world.y/8));
+			posDial.getLabel().text = "Place at " + Math.round(bPos.x) + ", " + Math.round(bPos.y);
+			dialog.show();
+		}, true);
+	}).width(250).get();
+	
+	table.row();
+	
+	dialog.addCloseButton();
+	
+	dialog.buttons.button("Place block", Icon.modeSurvival, place)
+	.disabled(() => !!Vars.state.sector);
+	
+	const iconCol = extend(TextureRegionDrawable, Tex.whiteui, {});
+	iconCol.tint.set(bTeam.color);
+	dialog.buttons.button("Set team", iconCol, 40, () => {
+		ui.select("Set team", Team.all, t => {
+			bTeam = t;
+			iconCol.tint.set(bTeam.color);
+		}, (i, t) => "[#" + t.color + "]" + t);
+	});
+});
+
+ui.addButton("Block placer", bType, () => {
+	dialog.show();
+}, b => { button = b.get() });
