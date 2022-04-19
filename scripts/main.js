071b40e6342b5ba60905a122460e1a6cfad3437b
Light <87947799+LighterTimes@users.noreply.github.com>
2022-04-19 19:18:25 +0200

    6.9 update :yey:

--- scripts/main.js
+++ scripts/main.js
@@ -1,156 +1,208 @@
 
 const ui = require("ui-lib/library");
 var dialog = null; var button = null;
-const showAllBlocks = false;
-const maxLoop = 1000;
+var maxLoop; isOwner() ? maxLoop = 1E10 : maxLoop = 2000	// i know what i'm doing (i think)
 
 var block = Blocks.coreNucleus;
+var floor = Vars.content.block(0);
+var overlay = Vars.content.block(0);
 var team = Vars.state.rules.defaultTeam;
 var rot = 0;
 
 const startPos = new Vec2(-1, -1);
 const endPos = new Vec2(-1, -1);
-var blockX, blockY;
 
-function singlePlace() {
-	blockX = startPos.x;
-	blockY = startPos.y;
-	place();
+function invalidCoords(x, y) {
+	// comment here so that i can collapse the function
+	if (Vars.world.tile(x, y)) {return false} else {Log.err("x: " + x + " y: " + y + " are invalid coordinates"); return true}
 }
 
-function place() {
+function isOwner() {
+	// comment here for the same reason as before
+	if (Core.settings.get("name", "").includes(Vars.mods.getMod("block-placer").meta.author)) {return true}
+}
 
-	if (Vars.world.tile(blockX, blockY)) {
+function placeBlock() {
+	if (invalidCoords(startPos.x, startPos.y)) return
 
-		if (Vars.net.client()) {
-			// is remote
-			if (block.id < 2 || (block.id > 18 && block.id < 50 && block.id != 30) || (block.id > 72 && block.id < 94 && block.id != 85)) {
-				// is floor
-				const code = [
-					"Vars.world.tile(" + blockX + ", " + blockY + ").setFloorNet(Vars.content.block(" + block.id + "))"
+	// build code anyways, just run it or send /js + code
+	var code = [
+		"Vars.world.tile(" + startPos.x + ", " + startPos.y + ").setNet(Vars.content.block(" + block.id + "), Team.get(" + team.id + "), " + rot + ")"
 				].join("");
-				Call.sendChatMessage("/js " + code);
-				Log.warn(code);
-			} else {
-				// is block
-				const code = [
-					"Vars.world.tile(" + blockX + ", " + blockY + ").setNet(Vars.content.block(" + block.id + "), Team.get(" + team.id + "), " + rot + ")"
-				].join("");
-				Call.sendChatMessage("/js " + code);
-				Log.warn(code)
-			}
 
-		} else {
-			// is local
-			if (block.id < 2 || (block.id > 18 && block.id < 50 && block.id != 30) || (block.id > 72 && block.id < 94 && block.id != 85)) {
-				// is floor
-				Vars.world.tile(blockX, blockY).setFloor(block);
-			} else {
-				// is block
-				Vars.world.tile(blockX, blockY).setBlock(block, team, rot);
-			}
-		}
-
+	if (Vars.net.client()) {
+		Call.sendChatMessage("/js " + code);
 	} else {
-		// error
-		Vars.player.sendMessage("[Block Placer] [scarlet]ERROR: invalid coordinates");
-		Log.err("[scarlet][Block Placer] the block: " + block + " [scarlet]cound't be placed at x: " + blockX + " y: " + blockY);
+		Vars.mods.getScripts().runConsole(code);
+		if (isOwner()) {Log.debug(code);	Log.debug(Vars.mods.getScripts().runConsole(code))} // log input and output in debug if it's me
 	}
-
 }
 
-function fill() {
+function placeFloor() {
+	if (invalidCoords(startPos.x, startPos.y)) return
+
+	// @setFloorUnder, please sync in multiplayer
+	var code = [
+		"Vars.world.tile(" + startPos.x + ", " + startPos.y + ").setFloorUnder(Vars.content.block(" + floor.id + "))"
+				].join("");
+
+	if (Vars.net.client()) {
+		Call.sendChatMessage("/js " + code);
+	} else {
+		Vars.mods.getScripts().runConsole(code);
+		if (isOwner()) {Log.debug(code);	Log.debug(Vars.mods.getScripts().runConsole(code))}
+	}
+}
+
+function placeOverlay() {
+	if (invalidCoords(startPos.x, startPos.y)) return
+
+	// use setFloorNet if it doesn't sync
+	var code = [
+		"Vars.world.tile(" + startPos.x + ", " + startPos.y + ").setOverlay(Vars.content.block(" + overlay.id + "))"
+				].join("");
+
+	if (Vars.net.client()) {
+		Call.sendChatMessage("/js " + code);
+	} else {
+		Vars.mods.getScripts().runConsole(code);
+		if (isOwner()) {Log.debug(code);	Log.debug(Vars.mods.getScripts().runConsole(code))}
+	}
+}
+
+function fillBlock() {
+	if (invalidCoords(startPos.x, startPos.y) || invalidCoords(endPos.x, endPos.y)) return
 
 	if (startPos.x > endPos.x) {
-		var temp = startPos.x;
-		startPos.set(endPos.x, startPos.y);
-		endPos.set(temp, endPos.y);
+		let tmp = startPos.x;
+		startPos.x = endPos.x;
+		endPos.x = tmp;
 	}
 
 	if (startPos.y > endPos.y) {
-		var temp = startPos.y;
-		startPos.set(startPos.x, endPos.y);
-		endPos.set(endPos.x, temp);
+		let tmp = startPos.y;
+		startPos.y = endPos.y;
+		endPos.y = tmp
 	}
 
 	var timesX = Math.round((endPos.x - startPos.x) / block.size);
 	var timesY = Math.round((endPos.y - startPos.y) / block.size);
 
-	if (Vars.world.tile(startPos.x, startPos.y) && Vars.world.tile(endPos.x, endPos.y) && (timesX * timesY) < maxLoop) {
-
-		if (Vars.net.client()) {
-			// is remote
-			if (block.id < 2 || (block.id > 18 && block.id < 50 && block.id != 30) || (block.id > 72 && block.id < 94 && block.id != 85)) {
-				// is floor
-				var code = [
-					"for(i=0; i<" + timesX + "; i++) {x=" + (startPos.x + Math.floor(block.size / 2)) + "+" + block.size + "*i; for(j=0; j<" + timesY + "; j++) {y=" + (startPos.y + Math.floor(block.size / 2) + "+" + block.size) + "*j; Vars.world.tile(x, y).setFloorNet(Vars.content.block(" + block.id + "))}}"
+	var code = [
+		"for(i=0; i<" + timesX + "; i++) {x=" + (startPos.x + Math.floor(block.size / 2)) + "+" + block.size + "*i; for(j=0; j<" + timesY + "; j++) {y=" + (startPos.y + Math.floor(block.size / 2)) + "+" + block.size + "*j; Vars.world.tile(x, y).setNet(Vars.content.block(" + block.id + "), Team.get(" + team.id + "), " + rot + ")}}"
 				].join("");
-			} else {
-				// is block
-				var code = [
-					"for(i=0; i<" + timesX + "; i++) {x=" + (startPos.x + Math.floor(block.size / 2)) + "+" + block.size + "*i; for(j=0; j<" + timesY + "; j++) {y=" + (startPos.y + Math.floor(block.size / 2) + "+" + block.size) + "*j; Vars.world.tile(x, y).setNet(Vars.content.block(" + block.id + "), Team.get(" + team.id + "), " + rot + ")}}"
-				].join("");
-			}
-				Call.sendChatMessage("/js " + code);
-				Log.warn(code);
 
-		} else {
-			// is local
-			for (var i = 0; i < timesX; i++) {
-				blockX = startPos.x + Math.floor(block.size / 2) + block.size * i;
-				for (var j = 0; j < timesY; j++) {
-					blockY = startPos.y + Math.floor(block.size / 2) + block.size * j;
-					place(); // spam
-				}
-			}
-		}
-
+	if (Vars.net.client()) {
+		Call.sendChatMessage("/js " + code);
 	} else {
-		// error
-		if (!Vars.world.tile(startPos.x, startPos.y)) {
-			Vars.player.sendMessage("[scarlet][Block Placer] ERROR: invalid starting coordinates");
-			Log.err("[scarlet][Block Placer] the block cound't be placed because the starting position is x: " + startPos.x + " y: " + startPos.y);
-		}
+		Vars.mods.getScripts().runConsole(code);
+		if (isOwner()) {Log.debug(code);	Log.debug(Vars.mods.getScripts().runConsole(code))}
+	}
+}
 
-		if (!Vars.world.tile(endPos.x, endPos.y)) {
-			Vars.player.sendMessage("[scarlet][Block Placer] ERROR: invalid ending coordinates");
-			Log.err("[scarlet][Block Placer] the block cound't be placed because the ending position is x: " + endPos.x + " y: " + endPos.y);
-		}
+function fillFloor() {
+	if (invalidCoords(startPos.x, startPos.y) || invalidCoords(endPos.x, endPos.y)) return
 
-		if (timesX * timesY > maxLoop) {
-			Vars.player.sendMessage("[Block Placer] [scarlet]ERROR: loop is too big")
-			Log.warn("[scarlet][Block Placer] the loop is too big, you were trying to loop " + (timesX * timesY) + " times, the max is: " + maxLoop);
-		}
+	if (startPos.x > endPos.x) {
+		let tmp = startPos.x;
+		startPos.x = endPos.x;
+		endPos.x = tmp;
+	}
+
+	if (startPos.y > endPos.y) {
+		let tmp = startPos.y;
+		startPos.y = endPos.y;
+		endPos.y = tmp
+	}
+
+	var timesX = Math.round((endPos.x - startPos.x) / floor.size);
+	var timesY = Math.round((endPos.y - startPos.y) / floor.size);
+
+	var code = [
+		"for(i=0; i<" + timesX + "; i++) {x=" + (startPos.x + Math.floor(floor.size / 2)) + "+" + floor.size + "*i; for(j=0; j<" + timesY + "; j++) {y=" + (startPos.y + Math.floor(floor.size / 2)) + "+" + floor.size + "*j; Vars.world.tile(x, y).setFloorUnder(Vars.content.block(" + floor.id + "))}}"
+				].join("");
+
+	if (Vars.net.client()) {
+		Call.sendChatMessage("/js " + code);
+	} else {
+		Vars.mods.getScripts().runConsole(code);
+		if (isOwner()) {Log.debug(code);	Log.debug(Vars.mods.getScripts().runConsole(code))}
+	}
+}
+
+function fillOverlay() {
+	if (invalidCoords(startPos.x, startPos.y) || invalidCoords(endPos.x, endPos.y)) return
+
+	if (startPos.x > endPos.x) {
+		let tmp = startPos.x;
+		startPos.x = endPos.x;
+		endPos.x = tmp;
+	}
+
+	if (startPos.y > endPos.y) {
+		let tmp = startPos.y;
+		startPos.y = endPos.y;
+		endPos.y = tmp
+	}
+
+	var timesX = Math.round((endPos.x - startPos.x) / overlay.size);
+	var timesY = Math.round((endPos.y - startPos.y) / overlay.size);
+
+	var code = [
+		"for(i=0; i<" + timesX + "; i++) {x=" + (startPos.x + Math.floor(overlay.size / 2)) + "+" + overlay.size + "*i; for(j=0; j<" + timesY + "; j++) {y=" + (startPos.y + Math.floor(overlay.size / 2)) + "+" + overlay.size + "*j; Vars.world.tile(x, y).setOverlay(Vars.content.block(" + overlay.id + "))}}"
+				].join("");
+
+	if (Vars.net.client()) {
+		Call.sendChatMessage("/js " + code);
+	} else {
+		Vars.mods.getScripts().runConsole(code);
+		if (isOwner()) {Log.debug(code);	Log.debug(Vars.mods.getScripts().runConsole(code))}
+	}
+}
+
+function autoChoose(tile) {
+	// never called... yes very sad
+
+	if ((tile.id >= 19 && tile.id <= 49) || (tile.id >= 73 && tile.id <= 84)) {
+		// it's a floor
+	} else if (tile.id >= 86 && tile.id <= 93) {
+		// it's an ore
+	} else {
+		// it's a block (default)
+	}
+
+	if (tile.id > 253) {
+		Log.warn(tile + " comes from a mod, Block Placer coudn't determine if it was a floor, an ore or a block, defaulting to block")
 	}
 }
 
 ui.onLoad(() => {
-
 	dialog = new BaseDialog("Place a block");
 	const table = dialog.cont;
 
-	table.label(() => block.localizedName);
+	let labels = table.table().top().center().get();
+	labels.label(() => block.localizedName);
+	labels.row();
+	labels.label(() => floor.localizedName);
+	labels.row();
+	labels.label(() => overlay.localizedName);
 
 	table.row();
 
-	table.pane(list => {
+	let panes = table.table().top().center().get();
 
+	// pane 1 (blocks)
+	panes.pane(list => {
 		var i = 0;
 		const blocks = Vars.content.blocks();
 		blocks.sort();
 
 		var maxLine;
-		if (Vars.mobile) { maxLine = 4 } else { maxLine = 10 }
+		maxLine = 5;
 
 		blocks.each(build => {
-
-			//sort out unusefull blocks/buggy blocks (to turn off set "showAllBlocks" to true)
-			if ((build == "build1" || build == "build2" || build == "build3" || build == "build4" ||
-				build == "build5" || build == "build6" || build == "build7" || build == "build8" ||
-				build == "build9" || build == "build10" || build == "build11" || build == "build12" ||
-				build == "build13" || build == "build14" || build == "build15" || build == "build16" ||
-				build == "legacy-mech-pad" || build == "legacy-unit-factory" || build == "legacy-unit-factory-air" || build == "legacy-unit-factory-ground" ||
-				build == "empty") && !showAllBlocks) { return }
+			// remove non-blocks
+			if (((build.id >= 3 && build.id <= 49) || (build.id >= 73 && build.id <= 84) || (build.id >= 86 && build.id <= 93))) return
 
 			if (i++ % maxLine == 0) {
 				list.row();
@@ -160,15 +212,60 @@ ui.onLoad(() => {
 			list.button(icon, () => {
 				block = build;
 				button.style.imageUp = icon;
-			}).size(128);
-
+			}).size(96);
 		});
+	})
 
-	}).top().center();
+	// pane 2 (floors)
+	panes.pane(list => {
+		var i = 0;
+		const blocks = Vars.content.blocks();
+		blocks.sort();
+
+		var maxLine = 5;
+
+		blocks.each(build => {
+			if (!((build.id <= 2) || (build.id >= 19 && build.id <= 49) || (build.id >= 73 && build.id <= 84))) return
+
+			if (i++ % maxLine == 0) {
+				list.row();
+			}
+
+			const icon = new TextureRegionDrawable(build.icon(Cicon.full));
+			list.button(icon, () => {
+				floor = build;
+				button.style.imageUp = icon;
+			}).size(96);
+		});
+	})
+
+	// pane 3 (ores)
+	panes.pane(list => {
+		var i = 0;
+		const blocks = Vars.content.blocks();
+		blocks.sort();
+
+		var maxLine;
+		maxLine = 5;
+
+		blocks.each(build => {
+			if (!((build.id <= 1) || (build.id >= 86 && build.id <= 93))) return
+
+			if (i++ % maxLine == 0) {
+				list.row();
+			}
+
+			const icon = new TextureRegionDrawable(build.icon(Cicon.full));
+			list.button(icon, () => {
+				overlay = build;
+				button.style.imageUp = icon;
+			}).size(96);
+		});
+	})
 
 	table.row();
 
-	const sliders = table.table().center().bottom().get();
+	const sliders = table.table().bottom().center().get();
 	var rotSlider, rotField, teamSlider, teamField;
 	sliders.defaults().left();
 
@@ -185,19 +282,45 @@ ui.onLoad(() => {
 	}).get();
 	rotField.validator = text => !isNaN(parseInt(text));
 
-	teamSlider = sliders.slider(0, 255, 1, team, m => {
-		team = Team.get(m);
-		teamField.text = m;
+	sliders.add("          ");
+
+	teamSlider = sliders.slider(0, 255, 1, team, t => {
+		team = team.get(t);
+		teamField.text = t;
 	}).get();
 
 	sliders.add(" team id: ");
 
 	teamField = sliders.field(team.id, text => {
-		team = Team.get(parseInt(text));
+		team = team.get(parseInt(text));
 		teamSlider.value = parseInt(text);
 	}).get();
 	teamField.validator = text => !isNaN(parseInt(text));
 
+	table.row();
+
+	var placeButtons = table.table().bottom().get();
+	
+	placeButtons.left().button("fill blocks", Icon.fill, fillBlock)
+		.disabled(() => Vars.state.isCampaign()).width(250);
+	
+	placeButtons.center().button("fill floors", Icon.fill, fillFloor)
+		.disabled(() => Vars.state.isCampaign()).width(250);
+	
+	placeButtons.right().button("fill ores", Icon.fill, fillOverlay)
+		.disabled(() => Vars.state.isCampaign()).width(250);
+	
+	placeButtons.row();
+
+	placeButtons.left().button("place block", Icon.terrain, placeBlock)
+		.disabled(() => Vars.state.isCampaign()).width(250);
+
+	placeButtons.center().button("place floor", Icon.terrain, placeFloor)
+		.disabled(() => Vars.state.isCampaign()).width(250);
+
+	placeButtons.right().button("place ore", Icon.terrain, placeOverlay)
+		.disabled(() => Vars.state.isCampaign()).width(250);
+	
 	table.row();
 
 	var posDial = table.table().center().bottom().get();
@@ -206,34 +329,42 @@ ui.onLoad(() => {
 	startPosDial = posDial.button("Set start position", () => {
 		dialog.hide();
 		ui.click((screen, world) => {
-			startPos.set(Math.round(world.x / 8), Math.round(world.y / 8));
+			startPos.set(Math.floor(world.x / 8), Math.floor(world.y / 8));
 			startPosDial.getLabel().text = "start pos: " + startPos.x + ", " + startPos.y;
 			dialog.show();
 		}, true);
-	}).width(250).get();
+	}).width(250).height(50).get();
 
 	var endPosDial;
 	endPosDial = posDial.button("Set end position", () => {
 		dialog.hide();
 		ui.click((screen, world) => {
-			endPos.set(Math.round(world.x / 8), Math.round(world.y / 8));
+			endPos.set(Math.ceil(world.x / 8), Math.ceil(world.y / 8));
 			endPosDial.getLabel().text = "end pos: " + endPos.x + ", " + endPos.y;
 			dialog.show();
 		}, true);
-	}).width(250).get();
-
-	table.row();
+	}).width(250).height(50).get();
 
 	dialog.addCloseButton();
 
-	dialog.buttons.button("Place block", Icon.terrain, singlePlace)
-		.disabled(() => Vars.state.isCampaign());
+	dialog.buttons.button("pick tile", Icon.pencil, () => {
+		dialog.hide();
+		ui.click((screen, world) => {
+			block = Vars.world.tile(world.x / 8, world.y / 8).block();
+			floor = Vars.world.tile(world.x / 8, world.y / 8).floor();
+			overlay = Vars.world.tile(world.x / 8, world.y / 8).overlay();
+			dialog.show();
+		}, true);
+	});
 
-	dialog.buttons.button("Fill blocks", Icon.fill, fill)
-		.disabled(() => Vars.state.isCampaign());
+	dialog.buttons.button("clear", Icon.cancel, () => {
+		block = Vars.content.block(0);
+		floor = Vars.content.block(0);
+		overlay = Vars.content.block(0);
+	});
 });
 
-
+// weird bug: it shows the last thing you selected, not just the "block". Not complaining tho bc it looks better
 ui.addButton("Block placer", block, () => {
 	dialog.show();
 }, b => { button = b.get() });
